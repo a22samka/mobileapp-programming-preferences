06ba6df1f74b5c72b4850b9b54b89d236bbf04f3
a22samka <a22samka@student.his.se>
2023-05-10 11:49:10 +0200

    Etablerade textView1 och button1, satte en onclick listener. Gjorde en onClick funktion. Samt en onResume funktion.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    SharedPreferences preferences;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        text = findViewById(R.id.textView1);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = preferences.getString("name", "hittades ej");
+        text.setText(name);
     }
 
 }
