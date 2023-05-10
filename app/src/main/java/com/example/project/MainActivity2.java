78226e9a6b4cd1a38307c5c98143032592bfe0fa
a22samka <a22samka@student.his.se>
2023-05-10 12:11:51 +0200

    Etablerade de olika widgetsen, satte en onclick listener. Gjorde även en onClick funktion.

--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -2,13 +2,41 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button tillbaks;
+    EditText namn;
+    SharedPreferences preferences;
+    Button knapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        namn = findViewById(R.id.editText);
+        knapp = findViewById(R.id.knappSpara);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        tillbaks = findViewById(R.id.knappTillbaka);
+        tillbaks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        knapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", namn.getText().toString());
+                editor.apply();
+            }
+        });
     }
 }
